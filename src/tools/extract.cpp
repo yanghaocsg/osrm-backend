37bd89c4b526918b64d47a2f43819593b4961481
Daniel Patterson <daniel@mapbox.com>
2016-10-25 14:22:06 -0600

    Don't mark all lanes as designated when there are blank lane specifiers in the tag.

--- src/tools/extract.cpp
+++ src/tools/extract.cpp
@@ -1,6 +1,6 @@
 #include "extractor/extractor.hpp"
 #include "extractor/extractor_config.hpp"
-#include "extractor/scripting_environment_lua.hpp"
+#include "extractor/scripting_environment_sol2.hpp"
 #include "util/simple_logger.hpp"
 #include "util/version.hpp"
 
@@ -151,7 +151,7 @@ int main(int argc, char *argv[]) try
     }
 
     // setup scripting environment
-    extractor::LuaScriptingEnvironment scripting_environment(
+    extractor::Sol2ScriptingEnvironment scripting_environment(
         extractor_config.profile_path.string().c_str());
     return extractor::Extractor(extractor_config).run(scripting_environment);
 }
